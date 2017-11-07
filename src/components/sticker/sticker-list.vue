a5493d9022c4d2bdec57f45e6dfdc3860d6abe90
Harvey.Wei <eplore@gmail.com>
2017-11-07 11:33:33 +0800

    素材管理

--- src/components/sticker/sticker-list.vue
+++ src/components/sticker/sticker-list.vue
@@ -5,13 +5,47 @@
       <li class="breadcrumb-item">分类列表</li>
     </ol>
 	<div class="biaoqing-container">
+
+    <ul class="biaoqing-nav nav nav-tabs nav-justified nav-line"  role="tablist">
+
+      <li class="nav-item ">
+        <a
+          :class="{active:(keyword=='1')}"
+          @click="tagSticker(1,'1')">
+          热门
+        </a>
+      </li>
+      <li class="nav-item ">
+        <a
+          :class="{active:(keyword=='0')}"
+          @click="tagSticker(1,'0')">
+          新品
+        </a>
+      </li>
+      <li class="nav-item " v-for="hot in hots.data">
+        <a
+          :class="{active:((keyword==hot.value) && (enable==1))}"
+          @click="tagSticker(1,hot.value)">
+          {{hot.value}}
+        </a>
+      </li>
+      <li class="nav-item ">
+        <a
+          :class="{active:(enable==false)}"
+          @click="goDelete">
+          回收站
+        </a>
+      </li>
+    </ul>
 		<div class="biaoqing-table">
 			<table class="table table-bordered table-hover">
 				<thead>
 		    		<tr>
 				      <th>封面</th>
 				      <th>name</th>
+				      <th>TAGS</th>
 				      <th>分类ID</th>
+				      <th>贴纸数量</th>
 				      <th>创建时间</th>
 				      <th>操作</th>
 				    </tr>
@@ -24,8 +58,14 @@
 		    				</div>
 		    			</td>
 		    			<td class="max-width20 biaoqing-table-content">{{materia.name}}</td>
+		    			<td class="max-width20 biaoqing-table-content">
+                <span @click="setTags(materia.id,index,materia.tags)" class="pass-success cursor" v-if="materia.tags" title="materia.tags">{{materia.tags}}</span>
+                <span @click="setTags(materia.id,index,materia.tags)" class="pass-ing cursor"
+                      v-if="(!materia.tags || materia.tags==' ')" title="设置"><i class="operation-icon fa fa-pencil"></i>设置</span>
+              </td>
 		    			<td>{{materia.id}}</td>
-		    			<td class="max-width100 publish-time">
+		    			<td>{{materia.stuffNum}}</td>
+		    			<td class="max-width20 publish-time">
 		    				<span>{{materia.createTime}}</span>
 		    			</td>
 		    			<td class="max-width20">
@@ -39,7 +79,7 @@
 			<nav v-if="materias.lastPageNumber!=1" aria-label="Page navigation example " class="">
 			  <ul class="pagination">
 			    <li v-if="!materias.firstPage" class="page-item">
-			      <a class="page-link" href="javascript:;" aria-label="Previous" 
+			      <a class="page-link" href="javascript:;" aria-label="Previous"
 			      @click="goMaterial(materias.prevPageNumber)">
 			        <span aria-hidden="true">&laquo;</span>
 			      </a>
@@ -47,7 +87,7 @@
 			    <!-- 回到第一页 -->
 			    <template v-if="(materias.pageNumber-4)>1">
 			    	<li class="page-item">
-				    	<a class="page-link" href="javascript:;"  
+				    	<a class="page-link" href="javascript:;"
 				    	@click="goMaterial(1)">1</a>
 				    </li>
 				    <li class="page-item disabled ">
@@ -56,7 +96,7 @@
 			    </template>
 			    <template v-for="page in materias.pageNumbers">
 				    <li class="page-item" :class="{active:(page==materias.pageNumber)}">
-				    	<a class="page-link" href="javascript:;"  
+				    	<a class="page-link" href="javascript:;"
 				    	@click="goMaterial(page)">{{page}}</a>
 				    </li>
 				</template>
@@ -66,7 +106,7 @@
 				    	<a class="page-link">...</a>
 				    </li>
 				    <li class="page-item">
-				    	<a class="page-link" href="javascript:;"  
+				    	<a class="page-link" href="javascript:;"
 				    	@click="goMaterial(materias.lastPageNumber)">{{materias.lastPageNumber}}</a>
 				    </li>
 			    </template>
@@ -83,8 +123,27 @@
 				</div>
 			  </ul>
 			</nav>
-	    </div>	
+	    </div>
 	</div>
+
+  <div class="selecTagContainer">
+    <div class="selecTag-bg" @click="selectCancle"></div>
+    <div class="selecTag-main">
+      <div class="selecTag-title">选择所展示的频道：</div>
+      <div class="clearfloat">
+        <div v-for="hot in hots"
+             class="hot-name"
+             :class="{active:(ch==hot.value)}"
+        >{{hot.value}}</div>
+      </div>
+      <div class="selecTag-btn-box">
+        <div class="selecTag-btn selecTag-cancle" @click="selectCancle">取消</div>
+        <div @click="setTagsView(categoryId,ch)" class="selecTag-btn selecTag-ok">确定</div>
+      </div>
+    </div>
+  </div>
+
+
 </div>
 
 </template>
@@ -99,25 +158,33 @@ export default {
 		loading: false,
 		formPage:'',
 		type:'',
-		materias:''
+    enable:'',
+		materias:'',
+    hots:'',
+    keyword:'',
+    categoryId:'',
+    ch:'',
+    index:''
 	}),
 	beforeRouteEnter (to,form,next) {
 		var params = {
 			pageSize:15,
 			pageNum:1,
-			type:1
+      enable:1,
+      keyword:'0'
 		}
 		Promise.all([StickerManage.category(params),StickerManage.materialHot()]).then(([materias,hots]) => {
-			console.log(materias)
 			for(var i = 0;i<materias.data.data.items.length;i++){
 				materias.data.data.items[i].createTime=formatTime(materias.data.data.items[i].createTime)
 			}
-    		next(vm => {
-    			vm.type=params.type;
-    			vm.materias = materias.data.data;
-    		})
+      next(vm => {
+        vm.enable=params.enable;
+        vm.keyword=params.keyword;
+        vm.hots = hots.data;
+        vm.materias = materias.data.data;
+      })
 		})
-		
+
 	},
     mounted () {
       this.$emit('loaded')
@@ -139,8 +206,103 @@ export default {
 				}
 				this.type=params.type;
 				this.materias=materias.data.data;
+				this.keyword = 0
 			})
     	},
+
+      setTags(id,index,tags){
+        this.categoryId = id;
+        this.index = index;
+        if(tags){
+          this.ch = tags;
+        }else{
+          this.ch = '';
+        }
+        $('.selecTagContainer').fadeIn('fast')
+      },
+      selectCancle(e){$('.selecTagContainer').fadeOut('fast')},
+      setTagsView(id,ch) {
+        var index = this.index;
+        var that = this;
+        var params = {
+          "id": id,
+          "tags": ch
+        }
+        if (ch != '') {
+          var msg = '设置成功'
+        } else {
+          var msg = '取消成功'
+        }
+        that.$http.post('/category/update', params).then(response => {
+          if (response.data.code == 200) {
+            var materias = that.materias;
+            materias.items[index].tags = params.tags;
+            toastr.success(msg);
+            that.ch = '';
+            that.categoryId = ''
+            $('.selecTagContainer').fadeOut('fast');
+          } else {
+            toastr.error(response.data.msg);
+          }
+        })
+      },
+      tagSticker(page,keyword) {
+        var params = {
+          pageSize: 15,
+          pageNum: page,
+          enable: 1
+        }
+        if(keyword=='最新'){
+          params.keyword='0';
+        }else if(keyword=='热门'){
+          params.keyword='1';
+        }else{
+          params.keyword=keyword;
+        }
+
+        this.$emit('loaded',true);
+        Promise.all([StickerManage.category(params)]).then(([materias]) => {
+          $('body,html').animate({scrollTop:0},10);
+          this.$emit('loaded',false);
+          console.dir(materias)
+          for(var i = 0;i<materias.data.data.items.length;i++){
+            materias.data.data.items[i].createTime=formatTime(materias.data.data.items[i].createTime)
+          }
+          console.dir(this)
+          this.init();
+          this.enable=1;
+          this.keyword=params.keyword;
+          this.materias=materias.data.data;
+        })
+      },
+
+      goDelete(){
+        var params = {
+          pageSize:15,
+          pageNum:1,
+          enable:0
+        }
+        this.$emit('loaded',true);
+        Promise.all([Subject.category(params)]).then(([materias]) => {
+          $('body,html').animate({scrollTop:0},10);
+          this.$emit('loaded',false)
+          for(var i = 0;i<materias.data.data.items.length;i++){
+            materias.data.data.items[i].createTime=formatTime(materias.data.data.items[i].createTime)
+          }
+          this.init();
+          this.enable=0;
+          this.materias=materias.data.data;
+        })
+      },
+
+      init(){
+        this.enable=''
+        this.you='';
+        this.keyword='';
+        this.ModelUserId='';
+        this.sort='';
+        this.asc='';
+      },
     	setMateria(type,id,index){
     		var data={
 			  "id": id,
@@ -154,9 +316,10 @@ export default {
 				}else{
 					this.$notice.error(res.data.msg)
 				}
-				
+
 			})
     	}
-    } 
+
+    }
 }
 </script>
